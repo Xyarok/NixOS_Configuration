e3df133251c903da4ecb95156c37a67696818867
Xyarok <126290197+Xyarok@users.noreply.github.com>
2025-08-09 16:52:47 +0000

    Add files via upload

--- systems/blueMouse.nix
+++ systems/blueMouse.nix
@@ -2,7 +2,7 @@
 
 {
   networking.hostName = "blueMouse"; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -14,21 +14,10 @@
   # Set your time zone.
   time.timeZone = "America/New_York";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
   # Touchpad support
-  services.xserver.libinput.enable = true;
+  #services.xserver.libinput.enable = true;
+
+  # blueMouse specific packages
+  environment.systemPackages = with pkgs; [
+  ];
 }
